1250743c86390f5939b5c0e238252447c7222e0e
Hitesh Navare <navarehitesh65@gmail.com>
2025-08-25 11:26:51 +0530

    added if the loading and not user then show text

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useRef, useState } from 'react'
 import { AddInterview } from './_components/AddInterview'
 import { InterviewCard, InterviewCardProps } from './_components/InterviewCard'
 import axios from 'axios'
+import { Loader } from './_components/Loading'
+import { useUser } from '@clerk/nextjs'
 
 type Interview = {
   id: string;
@@ -20,18 +22,26 @@ type Interview = {
 export default function page() {
 
   const [interviewData, setInterviewData] = useState<Interview[]>([]);
+  const [isLoading , setIsLoading] = useState(false);
+  const {user, isSignedIn} = useUser()
 
   useEffect(() =>{
+    setIsLoading(true);
     const getCards = async() =>{
+      try{
       const response = await axios.get('/api/gemini/generate')
-  
+      
       if(response){
         const data = response.data.interviews;
         setInterviewData(data);
+        setIsLoading(false);
       }
-    }
-    getCards();
-  }, [])
+    } catch(error){
+      console.log(error);
+   }
+  }
+   getCards();
+  }, [isSignedIn])
 
   return (
     <div className='mt-28 mx-5 mb-18 h-[150vh]'>
@@ -40,12 +50,17 @@ export default function page() {
      <section className='mt-10 md:w-[70vw] lg:max-w-[60vw] mx-auto'>
          <h2 className='text-primary text-xl lg:text-2xl font-poppins my-5'>Your Interviews</h2>
 
+         {!user && interviewData.length === 0 ?
+         <p className='text-accent text-lg'>You dont have any interviews created yet.....</p> :
+         isLoading ? <Loader /> : 
          <div className='gap-6 md:gap-12 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 h-[300px] mb-10'>
-          
           {interviewData.map((interview)=>(
            <InterviewCard key={interview.id} {...interview}
-           />
-         ))}</div>
+           /> 
+        ))} 
+         </div> 
+       }
+      
 
      </section>
 
